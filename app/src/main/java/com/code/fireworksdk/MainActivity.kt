98b133f524d689a75397214ba65be27be4422128
Kiran <kkona01@albertsons.com>
2022-04-14 22:49:05 +0530

    Updated the version code

--- app/src/main/java/com/code/fireworksdk/MainActivity.kt
+++ app/src/main/java/com/code/fireworksdk/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.code.fireworksdk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.code.fireworksdk.databinding.ActivityMainBinding
+import com.loopnow.fireworklibrary.AdLabelType
 import com.loopnow.fireworklibrary.FwSDK
+import com.loopnow.fireworklibrary.VideoFeedProperties
+import com.loopnow.fireworklibrary.VideoPlayerProperties
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +16,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+       VideoFeedProperties.displayAdLabel  = true
+        VideoFeedProperties.setAdLabel(AdLabelType.AD)
+        VideoPlayerProperties.enableOneTouchMute = true
+        VideoPlayerProperties.autoPlayOnComplete = false
+        VideoPlayerProperties.enablePlayPauseControl = false
+        VideoPlayerProperties.launchPlayerWithMute = true
         binding.grid.setPlaylist("","vJ0Yno")
         binding.grid.start()
 
@@ -34,40 +41,11 @@ class MainActivity : AppCompatActivity() {
         // Implement CtaClickHandler
 
         FwSDK.ctaClickHandler = object : FwSDK.CtaClickHandler {
-            /**
-             * return true if you handled, false if you want SDK to handle it.
-             */
+
             override fun ctaClicked(label: String, actionUrl: String?): Boolean {
                 return false
             }
         }
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
-
-    // Shows the system bars by removing all the flags
-    // except for the ones that make the content appear under the system bars.
-    private fun showSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-    }
 }
